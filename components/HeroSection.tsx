c1aeb9ffdfdd877119b3a74129d265869135dbb9
Danjuma Ashiwaju <danjumashiwaju@gmail.com>
2023-06-19 13:47:43 +0000

    Added responsive heroSection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,40 +1,50 @@
-import Image from "next/image"
-import { CustomButton, CustomText, CustomLottiePlayer } from "@/components"
-import heroLottie from "@/public/lottie-1.json"
-import { MdOutlinePlayCircleOutline } from "react-icons/md"
+import Image from "next/image";
+import { CustomButton, CustomText, CustomLottiePlayer } from "@/components";
+import heroLottie from "@/public/lottie-1.json";
+import { MdOutlinePlayCircleOutline } from "react-icons/md";
 
 /**
 TODO: Add responsiveness 
 */
 function HeroSection() {
-	return (
-		<section className="hero pt-44 pb-16 px-14  md:px-20 lg:px-32 flex justify-between flex-col md:flex-row ">
-			<div className="w-full md:w-lg text-center md:text-left grid justify-center">
-			{/*Heading*/}
-				<h1 className="text-6xl my-3 font-semibold ">
-					We’re here to Increase your Productivity
-				</h1>
-			{/*mark + text + button*/}
-				<div className="">
-					<Image src="/mark.svg" alt="just a mark" width={400} height={100} className="w-full" />
-					<CustomText
-						text="Let's make your work more organize and easily using the app 
+  return (
+    <section className="hero pt-44 pb-16  px-6 sm:px-14 md:px-20 lg:px-32 flex justify-between flex-col lg:flex-row gap-14">
+      <div className="text-center md:text-left grid content-center ">
+        {/*Heading*/}
+        <h1 className="text-5xl md:text-6xl my-3 font-semibold">
+          We’re here to Increase your Productivity
+        </h1>
+        {/*mark + text + button*/}
+        <div className="my-6 md:pr-26">
+          <Image
+            src="/mark.svg"
+            alt="just a mark"
+            width={400}
+            height={100}
+            className="w-full"
+          />
+          <div className="my-5">
+            <CustomText
+              text="Let's make your work more organize and easily using the app 
 						Dashboard with many of the latest features you need to succeed managing work daily."
-					/>
-			{/*Buttons */}
-				<div className="flex items-center gap-5">
-					<CustomButton title="Try free trial" />
-					<CustomButton
-						title="Watch a demo"
-						outline
-						icon={<MdOutlinePlayCircleOutline fontSize={"1.3rem"} />}
-					/>
-				</div>
-				</div>
-			</div>
-			<div className="hidden lg:flex"><CustomLottiePlayer src={heroLottie} /></div>
-		</section>
-	)
+            />
+          </div>
+          {/*Buttons */}
+          <div className="flex flex-col md:flex-row justify-center md:justify-start gap-5">
+            <CustomButton title="Try free trial" />
+            <CustomButton
+              title="Watch a demo"
+              outline
+              icon={<MdOutlinePlayCircleOutline fontSize={"1.3rem"} />}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="w-full">
+        <CustomLottiePlayer src={heroLottie} />
+      </div>
+    </section>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
--- components/ui/CustomButton.tsx
+++ components/ui/CustomButton.tsx
@@ -1,21 +1,21 @@
-import { CustomButtonProps } from "@/types"
+import { CustomButtonProps } from "@/types";
 
 export default function CustomButton({
-	title,
-	outline,
-	icon
+  title,
+  outline,
+  icon,
 }: CustomButtonProps) {
-	return (
-		<button
-			className={`px-5 py-2 rounded-full flex items-center gap-2 ${
-				!outline
-					? "bg-teal-500 text-white hover:bg-teal-800 "
-					: "bg-transparent text-teal-800  border border-teal-800  hover:bg-teal-800 hover:text-white"
-			}`}
-		>
-			{icon}
-			{title}
-			<span></span>
-		</button>
-	)
+  return (
+    <button
+      className={`px-5 py-2 rounded-full flex place-content-center gap-2 ${
+        !outline
+          ? "bg-teal-500 text-white hover:bg-teal-800 "
+          : "bg-transparent text-teal-800  border border-teal-800  hover:bg-teal-800 hover:text-white"
+      }`}
+    >
+      <span>{icon}</span>
+      {title}
+    </button>
+  );
 }
+
